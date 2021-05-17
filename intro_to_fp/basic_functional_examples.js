473fae052eee8423d83c36747daa760383f8191e
tkubrak <taras.kubrak@dataart.com>
2021-05-17 17:30:43 +0300

    feat: pure function exercises

--- intro_to_fp/basic_functional_examples.js
+++ intro_to_fp/basic_functional_examples.js
@@ -4,8 +4,18 @@
 //       its afternoon and "good morning" any other time of the day.
 // Hint - this will help with time of day: new Date().getHours()
 // -----------------------------------------------------------------
+function greeting() {
+    const time = new Date().getHours()
 
+    if(time > 12 && time < 17) {
+        return "Good Afternoon"
+    }
 
+    return "Good Morning!"
+}
+
+const greetingResult = greeting();
+console.log(greetingResult);
 
 
 // -----------------------------------------------------------------
@@ -14,5 +24,15 @@
 //       returns an array of items counting down from that number to 
 //       zero.
 // -----------------------------------------------------------------
+const countDownItemsFromNumber = (number) => {
+    let numbers = [];
 
+    for (let i = number; i >= 0; i--) {
+        numbers.push(i);
+    }
 
+    return numbers;
+}
+
+const numbersArr = countDownItemsFromNumber(7);
+console.log(numbersArr);
